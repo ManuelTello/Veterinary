d3bcf3a65d75b0b6755edda9180587f1524676d3
manuel <manuelTello.business@gmail.com>
2024-12-20 14:27:49 -0300

    Refactoring to use a connection string from the WebAPI layer instead of the Infrastructure hard coded one

--- Veterinary.Backend/Source/Veterinary.Backend.Infrastructure/DependencyInjection.cs
+++ Veterinary.Backend/Source/Veterinary.Backend.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Veterinary.Backend.Infrastructure.Persistence.Context;
 
@@ -6,20 +7,21 @@ namespace Veterinary.Backend.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            AddPersistence(services);
+            AddPersistence(services, configuration);
             return services;
         }
-        
-        private static IServiceCollection AddPersistence(this IServiceCollection services)
+
+        private static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectionString = configuration.GetConnectionString("ApplicationConnection") ?? throw new NullReferenceException();
+
             services.AddDbContext<ApplicationContext>(options =>
             {
-                options.UseSqlite("Data Source=Veterinary.db;Cache=Shared;Mode=ReadWriteCreate;");
+                options.UseSqlite(connectionString);
             });
             return services;
         }
     }
 }
-
--- Veterinary.Backend/Source/Veterinary.Backend.WebAPI/Program.cs
+++ Veterinary.Backend/Source/Veterinary.Backend.WebAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services
-    .AddInfrastructure();
+    .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
