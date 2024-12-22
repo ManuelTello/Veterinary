b9328834f7cc5dce43de5f719bd6ec1541535f7f
Manuel Tello Siri <manuelTello.business@gmail.com>
2024-12-22 13:19:55 -0300

    Name added to the Email domain model

--- Veterinary.Backend/Source/Veterinary.Backend.Domain/AggregateRoots/Owner/Owner.cs
+++ Veterinary.Backend/Source/Veterinary.Backend.Domain/AggregateRoots/Owner/Owner.cs
@@ -2,13 +2,25 @@ using Veterinary.Backend.Domain.ObjectValues;
 
 namespace Veterinary.Backend.Domain.AggregateRoots.Owner
 {
-    public sealed class Owner(Guid id, DateTime dateAddedToTheSystem, Email email)
+    public sealed class Owner
     {
-        public Guid Id { get; private set; } = id;
+        public Guid Id { get; private set; }
 
-        public DateTime DateAddedToTheSystem { get; private set; } = dateAddedToTheSystem;
+        public DateTime DateAddedToTheSystem { get; private set; }
 
-        public Email Email { get; private set; } = email;
+        public Email Email { get; private set; }
+
+        public Name Name { get; private set; }
+
+        public Owner(Guid id, DateTime dateAddedToTheSystem, Email email, Name name)
+        {
+            this.Id = id;
+            this.DateAddedToTheSystem = dateAddedToTheSystem;
+            this.Email = email;
+            this.Name = name;
+        }
+
+        private Owner() { }
     }
 }
 
--- Veterinary.Backend/Source/Veterinary.Backend.Domain/ObjectValues/Name.cs
+++ Veterinary.Backend/Source/Veterinary.Backend.Domain/ObjectValues/Name.cs
@@ -1,7 +1,44 @@
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using FluentResults;
+
 namespace Veterinary.Backend.Domain.ObjectValues
 {
     public partial record Name
     {
+        private const string RegexName = @"^([a-zA-Z]\s{1})+$";
 
+        private const int MaxLength = 250;
+
+        public string NameValue { get; init; }
+
+        private Name(string nameValue)
+        {
+            this.NameValue = nameValue;
+        }
+
+        public static Result<Name> Create(string nameValue)
+        {
+            if (string.IsNullOrEmpty(nameValue) || string.IsNullOrWhiteSpace(nameValue))
+            {
+                return Result.Fail("Field is required");
+            }
+            else if (nameValue.Length > MaxLength)
+            {
+                return Result.Fail("Name too long");
+            }
+            else if (!Validate().IsMatch(nameValue))
+            {
+                return Result.Fail("Invalid name");
+            }
+            else
+            {
+                var name = new Name(nameValue);
+                return name;
+            }
+        }
+
+        [GeneratedRegex(RegexName)]
+        private static partial Regex Validate();
     }
 }
